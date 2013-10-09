51c54c215a6940ddb1a3b0350e5317a3967a644f
unknown <tthomas2@tthomas-westbc1.am.trimblecorp.net>
2013-10-09 10:42:35 +0200

    Added initial version.

---
+++ src/safe_observer_events.rb
@@ -0,0 +1,177 @@
+# Mixin module providing methods to safely react with model changes to observer
+# events.
+#
+# @example Manually deferring model changes.
+#   class MySafeEntitiesObserver < Sketchup::EntitiesObserver
+#  
+#     include SafeObserverEvents
+#  
+#     def onElementAdded(entities, entity)
+#       # Cache some data.
+#       @cache ||= []
+#       @cache << entity
+#       # Change the model.
+#       defer_model_change(entity) {
+#         # Entity might already be invalid. Make sure to check for that.
+#         return if entity.deleted?
+#         if entity.is_a?(Sketchup::Face)
+#           entity.erase!
+#         else
+#           entity.set_attribute('MeMyselfAndI', 'LickIt', 'IOweIt!')
+#         end
+#       }
+#     end
+#  
+#   end # class
+#  
+#   observer = MySafeEntitiesObserver.new
+#   Sketchup.active_model.entities.add_observer(observer)
+#
+# @example Automatic Wrapping
+#   class MySafeEntitiesObserver < Sketchup::EntitiesObserver
+#   
+#     include SafeObserverEvents
+#   
+#     def safe_onElementAdded(entities, entity)
+#       # Entity might already be invalid. Make sure to check for that.
+#       return if entity.deleted?
+#       if entity.is_a?(Sketchup::Face)
+#         entity.erase!
+#       else
+#         entity.set_attribute('MeMyselfAndI', 'LickIt', 'IOweIt!')
+#       end
+#     end
+#   
+#   end # class
+#   
+#   observer = MySafeEntitiesObserver.new
+#   Sketchup.active_model.entities.add_observer(observer)
+module SafeObserverEvents
+
+  # Safely defer the execution of the block and wrap everything in an operation.
+  #
+  # @param [Sketchup::Model, #model]
+  # @return [Nil]
+  def defer_model_change(model, &block)
+    # Allow an object with a #model method to be used.
+    unless model.is_a?(Sketchup::Model)
+      if model.respond_to?(:model)
+        model = model.model
+      end
+    end
+    # Ensure we have a valid model before attemping to perform any action.
+    unless model.is_a?(Sketchup::Model) && model.valid?
+      raise ArgumentError, "Need a valid model (#{model.inspect})"
+    end
+    timer_id = UI.start_timer(0, false) {
+      # Opening a modal window will cause this non-repeating timer to repeat.
+      # to work around this we explicitly stop it, just to be safe.
+      UI.stop_timer(timer_id)
+      # Ensure that the operation is transparent so the Undo stack isn't
+      # cluttered.
+      #
+      # NOTE: In SketchUp 8 M1 and older the name of the operation would
+      #       change with transparent operations. No workaround for this.
+      #       Since SketchUp 8 M2 the name will remain unchanged.
+      model.start_operation('Observer Event Change', true, false, true)
+      #begin
+        block.call
+      #rescue => e
+        # (!) Do NOT abort the operation! Aborting transparent operations will
+        #     abort the previous one as well - not desired!
+        #model.abort_operation
+        #raise e
+      #end
+      model.commit_operation
+    }
+    nil
+  end
+
+  def self.included(target_module)
+
+    unless Sketchup.version.to_i > 6
+      raise "#{self} requires SketchUp 7 or higher"
+    end
+
+    # Forward observer events to safe wrapper. This is needed in case the
+    # observer didn't subclass an Observer prototype.
+    def method_missing(symbol, *args, &block)
+      safe_symbol = "safe_#{symbol}"
+      if respond_to?(safe_symbol)
+        model = find_model_argument(*args)
+        defer_model_change(model) {
+          send(safe_symbol, *args, &block)
+        }
+      else
+        super
+      end
+    end
+
+    # When the observer has subclasses a template observer we must inject
+    # forwarding methods that will ensure the safe wrappers are called.
+    target_module.instance_methods.grep(/on[A-Z]/).each { |symbol|
+      safe_symbol = "safe_#{symbol}"
+      target_module.class_eval {
+        define_method(symbol) { |*args|
+          # Call original method just in case a sub-class implements a non-safe
+          # event callback.
+          super(*args)
+          # Now trigger the safe method.
+          if respond_to?(safe_symbol)
+            model = find_model_argument(*args)
+            defer_model_change(model) {
+              send(safe_symbol, *args)
+            }
+          end
+        } # define method
+      }
+    }
+
+    # SketchUp might query the observer instance before calling it. We must
+    # intercept this and check if there is a safe method to handle the
+    # observer callback.
+    unless method_defined?(:safe_observer_event_respond_to_backup?)
+      alias :safe_observer_event_respond_to_backup? :respond_to?
+      def respond_to?(*args)
+        # First check if the object responds to that method.
+        unless safe_observer_event_respond_to_backup?(*args)
+          # If it doesn't then check if there is a safe_ version of the method.
+          args[0] = "safe_#{args[0]}"
+          return safe_observer_event_respond_to_backup?(*args)
+        end
+        true
+      end
+    end
+
+  end
+
+  private
+
+  # Forward the arguments from an observer to this utility method that will
+  # attempt to find the relevant model for the given entities/objects.
+  #
+  # @param [Mixed]
+  # @return [Sketchup::Model, Nil]
+  def find_model_argument(*args)
+    # First a valid model object is searched for.
+    model = args.find { |arg|
+      arg.is_a?(Sketchup::Model) && model.valid?
+    }
+    # Then we look for objects that might return a valid model object.
+    unless model
+      entity ||= args.find { |arg|
+        arg.respond_to?(:model) &&
+        arg.model.is_a?(Sketchup::Model) && arg.model.valid?
+      }
+      if entity
+        model = entity.model
+      end
+    end
+    # We should only return valid objects.
+    unless model && model.valid?
+      return nil
+    end
+    model
+  end
+
+end # module
